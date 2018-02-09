e04c136c7f7517a436f90b7ef40a48a0e0e40469
Alasdair McLeay <alasdair.mcleay@me.com>
2018-02-09 10:36:28 +0000

    use @penx/swagger-node-runner fork

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 'use strict';
 
-var SwaggerExpress = require('swagger-express-mw');
+// Patched version of swagger-node-runner waiting on https://github.com/theganyo/swagger-node-runner/pull/119
+var Runner = require('@penx/swagger-node-runner');
 var app = require('express')();
 var path = require('path');
 
 module.exports = app; // for testing
 
-function createApp({spec = './example/spec/petstore.json', mock, config = '.', port = process.env.PORT || 10010} = {}) {
+function registerApp(runner, port) {
+  const swaggerExpress = runner.expressMiddleware();
+  swaggerExpress.register(app);
+  app.listen(port);
+  console.log('Paths:')
+  console.log(Object.keys(swaggerExpress.runner.swagger.paths));
+  console.log('Server running at http://127.0.0.1:' + port);
+};
+
+function createApp({
+  spec = path.join(__dirname, './example/spec/petstore.json'),
+  mock,
+  config = path.join(__dirname, '.'),
+  port = process.env.PORT || 10010} = {}) {
   // If spec not defined, load example
   // If config not defined, load default
   var config = {
-    configDir:  path.join(__dirname, config),
-    swaggerFile: path.join(__dirname, spec),
+    configDir: config,
+    swaggerFile: spec,
     appRoot: __dirname
   };
 
-  SwaggerExpress.create(config, function(err, swaggerExpress) {
+  Runner.create(config, function(err, runner) {
     if (err) { throw err; }
-    swaggerExpress.register(app);
-    app.listen(port);
-    console.log('Paths:')
-    console.log(Object.keys(swaggerExpress.runner.swagger.paths));
-    console.log('Server running at http://127.0.0.1:' + port);
+    registerApp(runner, port);
   });
 
   return app;
--- cli.js
+++ cli.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var path = require('path');
 var {version} = require('./package.json');
 var app = require('./app.js');
+
+function resolvePath(relativePath) {
+  console.log(path.join(process.cwd(), relativePath))
+  return path.join(process.cwd(), relativePath);
+}
 
 program
   .version(version)
   .usage('[options] <spec>')
-  .option('-m, --mock <dir>', 'Mock directory')
-  .option('-c, --config <file>', 'Config file')
-  .option('-p, --port <n>', 'Port', parseInt);
+  .option('-m, --mock <dir>', 'path to mock directory', resolvePath)
+  .option('-c, --config <file>', 'path to config file', resolvePath)
+  .option('-p, --port <n>', 'port to start the mock server on', parseInt);
 
 program.parse(process.argv);
 
-const spec = program.args[0];
+const spec = program.args[0] ? resolvePath(program.args[0]) : undefined;
 const {mock, config, port} = program;
 
 app({spec, mock, config, port})
