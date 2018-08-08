448c8e3df30094b0af12ce11f9b7d9c6ab035699
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-08-08 18:55:22 +0100

    refactor basic test and add test for /v2/pet/1

--- app.test.js
+++ app.test.js
@@ -5,14 +5,28 @@ const app = initapp({
   mock: './example/mocks'
 });
 
-setTimeout(
-  () => request(app)
+const test = () => {
+  request(app)
     .get('/v2/store/order/1')
     .expect('Content-Type', /json/)
     .expect('Content-Length', '2')
-    .expect(200)
+    .expect(200, {})
     .end(function(err, res) {
       if (err) throw err;
-    }),
-    1000
-  )
+    });
+
+  request(app)
+    .get('/v2/pet/1')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .expect(res => {
+      console.warn(res.body);
+      const body = JSON.parse(res.body);
+      console.warn(body)
+    })
+    .end(function(err, res) {
+      if (err) throw err;
+    });
+};
+
+setTimeout(test, 1000);
